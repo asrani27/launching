6a9b306d337d5f1df313250ad6dc6f6b94cebc7e
asrani <asrani.27@gmail.com>
2022-06-22 11:05:01 +0800

    feat: added pwa

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const cacheName = "launching-aplikasi-banjarmasin-pintar"
+const assets = [
+    "/",
+    "/index.html",
+    "/background.jpg",
+    "/button.png",
+    "/logo.png",
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(cacheName).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
